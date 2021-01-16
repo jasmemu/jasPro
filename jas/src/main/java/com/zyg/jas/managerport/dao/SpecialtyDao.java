9fd96d7789ee68d9a32638f802739a8d4744c93f
hihollo <2585196949@qq.com>
2021-01-16 18:23:38 +0800

    de

--- jas/src/main/java/com/zyg/jas/managerport/dao/SpecialtyDao.java
+++
@@ -1,9 +0,0 @@
-package com.zyg.jas.managerport.dao;
-
-import com.zyg.jas.common.pojo.Specialty;
-
-import java.util.List;
-
-public interface SpecialtyDao {
-    public List<Specialty> selectAllSpeCialty();
-}
