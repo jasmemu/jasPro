051806ce58342a99a14d49db543322efad9fae15
hihollo <2585196949@qq.com>
2021-01-26 17:49:21 +0800

    li

--- jasweb/src/App.vue
+++ jasweb/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <el-button type="primary" v-on:click="devSys()" v-show="butShow">开发管理员端页面</el-button>
-    <el-button type="primary" v-on:click="devStu()"  v-show="butShow" >开发学生端页面</el-button>
     <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
@@ -14,10 +12,6 @@
     }
    },
    methods:{
-     devSys(){
-      this.butShow = !this.butShow,
-       this.$router.push("/SysMainPage")
-     }
    },
    mounted() {
    }
