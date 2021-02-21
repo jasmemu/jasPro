b6c639af723889e38048cf327ea553e81d7f6dd0
hihollo <2585196949@qq.com>
2021-02-21 18:01:00 +0800

    addword

--- jas/src/main/java/com/zyg/jas/common/pojo/Homework.java
+++ jas/src/main/java/com/zyg/jas/common/pojo/Homework.java
@@ -4,45 +4,57 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Homework implements Serializable {
-    private String hId;
+    private Integer hId;
 
+    private String hName;
     private String comId;
+    private Committee committee;
 
     private String courseId;
+    private Course course;
 
     private String hUrl;
 
-    @JsonFormat(pattern="yyyy-MM-dd",timezone="Locale.US")//后台的时间 格式化 发送到前台
-    @DateTimeFormat(pattern="yyyy-MM-dd")//接受前台的时间格式 传到后台的格式
-    private String publishDate;
+    private String hPath;
 
     @JsonFormat(pattern="yyyy-MM-dd",timezone="Locale.US")//后台的时间 格式化 发送到前台
     @DateTimeFormat(pattern="yyyy-MM-dd")//接受前台的时间格式 传到后台的格式
-    private String endDate;
+    private Date publishDate;
 
-    private Integer id;
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="Locale.US")//后台的时间 格式化 发送到前台
+    @DateTimeFormat(pattern="yyyy-MM-dd")//接受前台的时间格式 传到后台的格式
+    private Date endDate;
+
+    private String mark;
+
 
     public Homework() {
     }
 
-    public Homework(String hId, String comId, String courseId, String hUrl, String publishDate, String endDate, Integer id) {
+
+    public Homework(Integer hId, String hName, String comId, Committee committee, String courseId, Course course, String hUrl, String hPath, Date publishDate, Date endDate, String mark) {
         this.hId = hId;
+        this.hName = hName;
         this.comId = comId;
+        this.committee = committee;
         this.courseId = courseId;
+        this.course = course;
         this.hUrl = hUrl;
+        this.hPath = hPath;
         this.publishDate = publishDate;
         this.endDate = endDate;
-        this.id = id;
+        this.mark = mark;
     }
 
-    public String gethId() {
+    public Integer gethId() {
         return hId;
     }
 
-    public void sethId(String hId) {
-        this.hId = hId == null ? null : hId.trim();
+    public void sethId(Integer hId) {
+        this.hId = hId;
     }
 
     public String getComId() {
@@ -69,40 +81,76 @@ public class Homework implements Serializable {
         this.hUrl = hUrl == null ? null : hUrl.trim();
     }
 
-    public String getPublishDate() {
+    public Date getPublishDate() {
         return publishDate;
     }
 
-    public void setPublishDate(String publishDate) {
-        this.publishDate = publishDate == null ? null : publishDate.trim();
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
     }
 
-    public String getEndDate() {
+    public Date getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
-        this.endDate = endDate == null ? null : endDate.trim();
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
 
-    public Integer getId() {
-        return id;
+    public String getMark() {
+        return mark;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String gethPath() {
+        return hPath;
+    }
+
+    public void sethPath(String hPath) {
+        this.hPath = hPath;
+    }
+
+    public Committee getCommittee() {
+        return committee;
+    }
+
+    public void setCommittee(Committee committee) {
+        this.committee = committee;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String gethName() {
+        return hName;
+    }
+
+    public void sethName(String hName) {
+        this.hName = hName;
     }
 
     @Override
     public String toString() {
         return "Homework{" +
-                "hId='" + hId + '\'' +
+                "hId=" + hId +
+                ", hName='" + hName + '\'' +
                 ", comId='" + comId + '\'' +
+                ", committee=" + committee +
                 ", courseId='" + courseId + '\'' +
+                ", course=" + course +
                 ", hUrl='" + hUrl + '\'' +
-                ", publishDate='" + publishDate + '\'' +
-                ", endDate='" + endDate + '\'' +
-                ", id=" + id +
+                ", hPath='" + hPath + '\'' +
+                ", publishDate=" + publishDate +
+                ", endDate=" + endDate +
+                ", mark='" + mark + '\'' +
                 '}';
     }
 }
