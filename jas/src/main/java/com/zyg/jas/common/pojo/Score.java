5597adcd7f099c44f78afef8546689ef009fec71
hihollo <2585196949@qq.com>
2021-03-03 19:17:05 +0800

    lxm0303

--- jas/src/main/java/com/zyg/jas/common/pojo/Score.java
+++ jas/src/main/java/com/zyg/jas/common/pojo/Score.java
@@ -1,20 +1,29 @@
 package com.zyg.jas.common.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
 import java.util.Date;
 
-public class Score {
-    private Integer id;
+public class Score implements Serializable {
+    private Integer id; // 学生提交的作业的id
 
-    private Integer hId;
+    private Integer hId; // 学委发布的作业id(homework表的主键)
+    private Homework homework;
 
     private String sNo;
+    private Student student;
 
     private String comId;
+    private Committee committee;
 
     private Double score;
 
     private String comment;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="Asia/Shanghai")//后台的时间 格式化 发送到前台
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")//接受前台的时间格式 传到后台的格式
     private Date correctDate;
 
     private String commitedJobUrl;
@@ -91,5 +100,47 @@ public class Score {
 
     public void setCommitedJobPath(String commitedJobPath) {
         this.commitedJobPath = commitedJobPath == null ? null : commitedJobPath.trim();
+    }
+
+    public Homework getHomework() {
+        return homework;
+    }
+
+    public void setHomework(Homework homework) {
+        this.homework = homework;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Committee getCommittee() {
+        return committee;
+    }
+
+    public void setCommittee(Committee committee) {
+        this.committee = committee;
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "id=" + id +
+                ", hId=" + hId +
+                ", homework=" + homework +
+                ", sNo='" + sNo + '\'' +
+                ", student=" + student +
+                ", comId='" + comId + '\'' +
+                ", committee=" + committee +
+                ", score=" + score +
+                ", comment='" + comment + '\'' +
+                ", correctDate=" + correctDate +
+                ", commitedJobUrl='" + commitedJobUrl + '\'' +
+                ", commitedJobPath='" + commitedJobPath + '\'' +
+                '}';
     }
 }
