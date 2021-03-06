7cad475098cea4b2442330ccfcb9fcb7af79a837
hihollo <2585196949@qq.com>
2021-03-06 18:01:16 +0800

    addjquery

--- jasweb/vue.config.js
+++ jasweb/vue.config.js
@@ -1,7 +1,9 @@
+const webpack = require('webpack');
 // 使用代理下载文件（当点击浏览器的刷新建时有异常）
 module.exports = {
     publicPath: '/',
     devServer: {
+        port: 8090,     // 项目占用的端口
         proxy: {
             '/exceltable': {  // excel模板下载使用
                 target: 'http://localhost:8081', // 要跨域的域名  (部署时)
@@ -16,5 +18,14 @@ module.exports = {
                 changeOrigin: true// 是否开启跨域
             }
         }
-    }
+    },
+    configureWebpack: {//引入jquery
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "windows.jQuery": "jquery"
+            })
+        ]
+    },
 }
